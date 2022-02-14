d0d8956e249a19ec188b0107cd5ae71124ed098d
Baisalbek Urustanbekov <baisalbek4@gmail.com>
2022-02-14 22:47:49 +0600

    Min Heap using PriorityQueue

--- src/Top_K_Elements/KLargestNumbers.java
+++ src/Top_K_Elements/KLargestNumbers.java
@@ -7,7 +7,7 @@ import java.util.PriorityQueue;
 public class KLargestNumbers {
 
     public static void main(String[] args) {
-        List<Integer> result = KLargestNumbers.findKLargestNumbers(new int[]{3, 1, 5, 12, 2, 11}, 0);
+        List<Integer> result = KLargestNumbers.findKLargestNumbers(new int[]{3, 1, 5, 12, 2, 11}, 3);
         System.out.println("Here are the top K numbers: " + result);
 
         result = KLargestNumbers.findKLargestNumbers(new int[]{5, 12, 11, -1, 12}, 3);
---
+++ src/Top_K_Elements/MinHeap.java
@@ -0,0 +1,29 @@
+package Top_K_Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class MinHeap {
+    public static void main(String[] args) {
+        int result = findKLargestNumbers(new int[]{3, 1, 5, 12, -3, 2, 11});
+        System.out.println("Here are the top K numbers: " + result);
+
+        result = findKLargestNumbers(new int[]{5, 12, 11, -1, 12});
+        System.out.println("Here are the top K numbers: " + result);
+    }
+
+    private static int findKLargestNumbers(int[] arr) {
+
+        // Constractor which compares n1 vs n2 and return -1, 0 or 1 (lamda)
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
+
+        // put first 'K' numbers in the min heap
+        for(int i=0; i<arr.length; i++){
+            minHeap.add(arr[i]);
+        }
+
+
+        return  minHeap.peek();
+    }
+}
